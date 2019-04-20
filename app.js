b480ce34c9f599edaf63790d65ad313f0f44ff40
Joao Jesus <joao.jesus@asos.com>
2019-04-20 12:37:38 +0100

    Refactor

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 require('dotenv').config();
 
-// Load app server using express
-const express = require('express');
 const morgan = require('morgan');
-const app = express();
-const mysql = require('mysql');
-
+const app = require('express')();
+const bodyParser = require('body-parser');
 const port = 3000;
-const foodDataTable = 'eatwell_fooddata';
-
-const connection =
-    mysql.createConnection(
-        {
-            host: `${process.env.HOST}`,
-            user: `${process.env.USER}`,
-            database: `${process.env.DATABASE}`,
-            password: `${process.env.PASSWORD}`,
-        });
+const { connection, statusCheck } = require('./database/db');
+const queries = require('./database/queries');
 
 app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/api/status', (req, res) => {
+    console.log('API CALL: /api/status');
+    statusCheck(req, res);
+});
 
 app.get('/', (req, res) => {
     console.log(`Responding to root route`);
@@ -26,34 +23,17 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-app.get('/food/:id', (req, res) => {
+app.get('/api/food/:id', (req, res) => {
     console.log(`Fetching food with id: ${req.params.id}...`);
-    connection.query(`SELECT * FROM ${foodDataTable} WHERE id = ${req.params.id};`, (err, rows, fields) => {
-        if (!err) {
-            console.log('Fetched data!!');
-            res.json(rows);
-            res.end();
-        }   else {
-            console.log(err);
-            res.end();
-        }
-    });
+    connection(req, res, queries.foodById(req.params.id));
 });
 
-app.get('/food', (req, res) => {
+app.get('/api/foods', (req, res) => {
     console.log('Fetching foods...');
-    connection.query(`SELECT * FROM ${foodDataTable};`, (err, rows, fields) => {
-        if (!err) {
-            console.log('Fetched data!!');
-            res.json(rows);
-            res.end();
-        } else {
-            console.log(err);
-            res.end();
-        }
-    });
+    connection(req, res, queries.foods);
 });
 
 app.listen(port, () => {
   console.log(`Server is up and listening on ${port}`);
 });
+
