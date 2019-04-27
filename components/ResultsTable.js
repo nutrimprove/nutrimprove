ec7ab2c67e600a3180a36f433dffad1070be2fe1
Joao Jesus <joao.jesus@asos.com>
2019-04-27 15:24:45 +0100

    Added results table with Material UI and some tweaks

---
+++ components/ResultsTable.js
@@ -0,0 +1,77 @@
+import PropTypes from 'prop-types';
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+        minWidth: 700,
+    },
+    table: {
+        minWidth: 700,
+    },
+    cell: {
+        padding: 10,
+    },
+    name: {
+        minWidth: 100,
+        padding: 10,
+    }
+});
+
+const ResultsTable = (props) => {
+    const {classes, values} = props;
+
+    if (values && values.length > 0) {
+        return (
+            <div id='results'>
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className={classes.cell}>id</TableCell>
+                                <TableCell className={classes.name}>Food name</TableCell>
+                                <TableCell className={classes.cell}>Portion (g)</TableCell>
+                                <TableCell className={classes.cell}>Portion (palm)</TableCell>
+                                <TableCell className={classes.cell}>Carbohydrates</TableCell>
+                                <TableCell className={classes.cell}>Fat</TableCell>
+                                <TableCell className={classes.cell}>Protein</TableCell>
+                                <TableCell className={classes.cell}>Sugar</TableCell>
+                                <TableCell className={classes.cell}>Salt</TableCell>
+                                <TableCell className={classes.cell}>Fibre</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {values.map(value => (
+                                <TableRow key={value.id}>
+                                    {Object.keys(value).map(key => (
+                                        <TableCell key={value[key]} className={classes.cell}>
+                                            {value[key]}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </div>
+        )
+    } else {
+        return (
+            <div id='results'/>
+        )
+    }
+};
+
+ResultsTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ResultsTable);
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import ResultsTable from "../components/ResultsTable";
 
-export default function Index() {
-    return (
-        <Layout>
-            <h1>Eatwell</h1>
-        </Layout>
-    )
-}
+const Index = (props) => (
+    <Layout>
+        <div id='foodQueryForm'>
+            <div id='idInput'>
+                <label htmlFor='foodByID_input'>Food by ID: </label>
+                <input id='foodByID_input' type="text"/>
+            </div>
+            <div id='nameInput'>
+                <label htmlFor='foodByName_input'>Food by name: </label>
+                <input id='foodByName_input' type='text'/>
+            </div>
+            <div id='formButtons'>
+                <input className='submit' id='getButton' type='submit' value='Get food'/>
+                <input className='submit' id='getAllButton' type='submit' value='Get all foods'/>
+            </div>
+        </div>
+        <ResultsTable values={props.foods}/>
+    </Layout>
+);
+
+Index.getInitialProps = async function () {
+    const res = await fetch('http://localhost:3000/api/v1/foods');
+    const data = await res.json();
+
+    console.log(`${data.value.length} foods retrieved.`);
+
+    return {
+        foods: null,
+    }
+};
+
+export default Index;
