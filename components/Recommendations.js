6603baf13003b62948356c96bff4e98d9c50478e
Joao Jesus <joao.jesus@asos.com>
2019-05-02 21:10:57 +0100

    Added recommendations

---
+++ components/Recommendations.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Button from "@material-ui/core/Button";
+import RecommendationsResults from "./RecommendationsResults";
+
+const buttonStyles = {
+   verticalAlign: 'bottom',
+   marginLeft: 10,
+};
+
+class TextFields extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         values: [],
+      };
+   }
+
+   fetchFoods = (endpoint) => {
+      console.log(JSON.stringify(endpoint));
+      return fetch(endpoint)
+         .then(response => response.json())
+         .then(data => {
+            this.setState({values: data.value});
+         });
+   };
+
+   updateResults = async () => {
+      await this.fetchFoods(`/api/v1/recommendations`);
+   };
+
+   render() {
+      const {values} = this.state;
+      return (
+         <form>
+            <Button
+               style={buttonStyles}
+               variant="contained"
+               color="primary"
+               onClick={this.updateResults}>
+               Fetch all recommendations
+            </Button>
+            <RecommendationsResults values={values}/>
+         </form>
+      )
+   }
+}
+
+export default TextFields;
---
+++ routing/recommendations.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {connection} = require('../database/db');
+const queries = require('../database/queries');
+
+const apiPath = '/api/v1';
+
+router.get(`${apiPath}/recommendations`, (req, res) => {
+   connection(req, res, queries.recommendations);
+});
+
+module.exports = router;
