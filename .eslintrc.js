faba6f1900380c160ac739dbd03995e3993743ee
mulholio <james@jmulholland.com>
2019-05-25 12:47:04 +0100

    various eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 9,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
   plugins: ['jsx-a11y', 'import'],
   extends: [
     'standard',
@@ -7,4 +15,7 @@ module.exports = {
     'plugin:jsx-a11y/recommended',
     'eslint-config-prettier', // later plugins override earlier ones so keep this near the bottom
   ],
+  rules: {
+    'react/react-in-jsx-scope': 0, // Not needed with Next.js
+  },
 }
