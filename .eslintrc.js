f6a5395ba5d99433c73c0b6b75b36a72e2be935b
mulholio <james@jmulholland.com>
2019-05-25 12:28:10 +0100

    add eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
-  "plugins": [
-    "jsx-a11y",
-    "import",
-  ],
-  "extends": [
-    "standard",
-    "plugin:react/recommended",
-    "plugin:import/warnings",
-    "plugin:jsx-a11y/recommended",
+  plugins: ['jsx-a11y', 'import'],
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier', // later plugins override earlier ones so keep this near the bottom
   ],
 }
