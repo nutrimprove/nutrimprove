ed75a7b79b1974d2c6db0e62f582bd0b1054fc62
mulholio <james@jmulholland.com>
2019-05-25 12:16:56 +0100

    add jsx-a11y and import plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
+  "plugins": [
+    "jsx-a11y",
+    "import",
+  ],
   "extends": [
     "standard",
     "plugin:react/recommended",
+    "plugin:import/warnings",
+    "plugin:jsx-a11y/recommended",
   ],
 }
