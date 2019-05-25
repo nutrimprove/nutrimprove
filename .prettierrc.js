7d33dbbd1ef5a4634203249c3235b457ec9d0d29
mulholio <james@jmulholland.com>
2019-05-25 12:24:43 +0100

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 75,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+}
