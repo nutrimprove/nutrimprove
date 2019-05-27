60fdaece2ed3d770483599c696672c8b78249b3d
mulholio <james@jmulholland.com>
2019-05-27 16:32:13 +0100

    infra: run tests on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,6 @@ module.exports = {
   linters: {
     '*.+(js|jsx|ts|tsx)': ['eslint'],
     '*.+(js|jsx|json|css|md|graphql|mdx)': ['prettier --write', 'git add'],
+    '**/.{js,jsx}': ['jest --findRelatedTests --bail'],
   },
 };
