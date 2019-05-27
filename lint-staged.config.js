eca42e4c70d80acf40aefecd17f11d162009cbd8
mulholio <james@jmulholland.com>
2019-05-27 16:34:36 +0100

    fix: typo in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
   linters: {
     '*.+(js|jsx|ts|tsx)': ['eslint'],
     '*.+(js|jsx|json|css|md|graphql|mdx)': ['prettier --write', 'git add'],
-    '**/.{js,jsx}': ['jest --findRelatedTests --bail'],
+    '**/*.{js,jsx}': ['jest --bail --findRelatedTests'],
   },
 };
