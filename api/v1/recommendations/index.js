43b9565415c3c704df6cb69f87e986509b3d78a3
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-07-10 18:41:43 +0100

    recommendations api returns results from mongoDB

--- api/v1/recommendations/index.js
+++ api/v1/recommendations/index.js
@@ -1,9 +1,11 @@
-const db = require('../../../lib/db');
-const escape = require('sql-template-strings');
+import { connectToDatabase } from '../../../lib/db';
 
-const recommendationsQuery = escape`SELECT rec.id, fd.foodname, fd2.foodname as "recommendation", cont.name FROM eatwell_recommendations rec, eatwell_fooddata fd, eatwell_fooddata fd2, eatwell_contributors cont WHERE rec.food_id = fd.id AND rec.foodrec_id=fd2.id AND rec.contributor_id=cont.id;`;
+const getRecommendations = async (req, res) => {
+  const mongodb = await connectToDatabase(process.env.MONGODB_URI);
+  const collection = await mongodb.collection('recommendations');
+  const recommendations = await collection.find({}).toArray();
 
-module.exports = async (req, res) => {
-  const recommendations = await db.query(recommendationsQuery);
-  res.end(JSON.stringify({ value: recommendations }));
+  return res.status(200).json({ recommendations });
 };
+
+export default getRecommendations;
