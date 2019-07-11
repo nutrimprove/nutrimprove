a96e5354b3b98d816f1dd1df527f292e52f2a888
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-07-11 22:28:31 +0100

    Ability to search all terms with '*'

--- api/v1/search/index.js
+++ api/v1/search/index.js
@@ -1,13 +1,19 @@
 import { connectToDatabase } from '../../../connect/db';
 
 const getCollectionResults = async (req, res) => {
-  const db = await connectToDatabase(process.env.MONGODB_URI);
-  const collection = await db.collection('search_cache');
-  const result = req.query.term
-    ? await collection.findOne({ search_term: req.query.term })
-    : await collection.find({}).toArray();
+  const term = req.query.term;
 
-  return res.status(200).json({ result });
+  if (term) {
+    const db = await connectToDatabase(process.env.MONGODB_URI);
+    const collection = await db.collection('search_cache');
+
+    const result =
+      term === '*'
+        ? await collection.find({}).toArray()
+        : await collection.findOne({ search_term: term });
+
+    return res.status(200).json({ result });
+  }
 };
 
 export default getCollectionResults;
