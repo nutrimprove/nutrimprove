e696a0a0dca4f6c93096572de4e692a15720b314
Joao Jesus <joao.jesus@river-island.com>
2019-07-14 12:35:51 +0100

    Removed post.js

--- api/v1/search/post.js
+++
@@ -1,8 +0,0 @@
-import { addSearchTerm } from '../../../connect/db';
-
-const saveSearchTerm = async (req, res) => {
-  const saved = await addSearchTerm(req.body, res);
-  return saved;
-};
-
-export default saveSearchTerm;
