f79d41b693982a603e024d4eb247ae2ce641454a
Siim Schults <siim.schults@gmail.com>
2019-07-22 23:37:46 +0100

    redux added, currently with example code from https://github.com/kirill-konshin/next-redux-wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import Page from '../components/Page';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Page from '../components/Page'
+import PropTypes from 'prop-types'
 
 const layoutStyle = {
   margin: 20,
   padding: 20,
   border: '1px solid #DDD',
   minWidth: 800,
-};
+}
 
-const Index = () => {
-  return (
-    <div id='app' style={layoutStyle}>
+class Index extends Component {
+  static getInitialProps({store, isServer, pathname, query}) {
+    store.dispatch({type: 'FOO', payload: 'foo'}); // component will be able to read from store's state when rendered
+    return {custom: 'custom'}; // you can pass some custom props to component from here
+  }
+
+  render () {
+    return (<div id='app' style={layoutStyle}>
       <Page />
-    </div>
-  );
+      {/* FIXME: remove following. shows how to use */}
+      <div>Prop from Redux {this.props.foo}</div>
+      <div>Prop from getInitialProps {this.props.custom}</div>
+    </div>);
+  }
+}
+
+Index.propTypes = {
+  foo: PropTypes.string.isRequired,
+  custom: PropTypes.string.isRequired,
 };
 
-export default Index;
+export default connect(state => state)(Index)
