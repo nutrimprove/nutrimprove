267d1a2e8153c2d5d23a6ced841cb2bea9da25a9
Joao Jesus <joao.jesus@river-island.com>
2019-08-13 16:41:33 +0100

    Updated dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  linters: {
-    '*.+(js|jsx|ts|tsx)': ['eslint'],
-    '*.+(js|jsx|json|css|md|graphql|mdx)': ['prettier --write', 'git add'],
-    '**/*.{js,jsx}': ['jest --bail --findRelatedTests'],
-  },
+  '*.+(js|jsx|ts|tsx)': ['eslint'],
+  '*.+(js|jsx|json|css|md|graphql|mdx)': ['prettier --write', 'git add'],
+  '**/*.{js,jsx}': ['jest --bail --findRelatedTests'],
 };
