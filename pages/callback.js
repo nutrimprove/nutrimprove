14b1a2597c488fb26230fd781593297237ae85e3
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-09-07 15:53:36 +0100

    Refactored callback class to be a functional component

--- pages/callback.js
+++ pages/callback.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import Auth from '../lib/Auth';
 import Router from 'next/router';
 import Header from '../components/Header';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 const auth = new Auth();
 
-export default class Callback extends Component {
-  componentDidMount() {
+const styles = {
+  message: {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100vw',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
+  },
+};
+
+const Callback = ({ classes }) => {
+  useEffect(() => {
     const userDetails = auth.extractInfoFromHash();
     auth.handleAuthentication().then(res => {
       if (!res) {
@@ -14,26 +31,18 @@ export default class Callback extends Component {
       }
       Router.push('/');
     });
-  }
+  }, []);
 
-  render() {
-    const style = {
-      position: 'absolute',
-      display: 'flex',
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100vw',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'white',
-    };
-    return (
-      <>
-        <Header />
-        <div style={style}>Processing login information!!</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header />
+      <div className={classes.message}>Processing login information!!</div>
+    </>
+  );
+};
+
+Callback.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Callback);
