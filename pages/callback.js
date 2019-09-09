fd0e311347e97b7a0eb8d0565ffe53a6a21aad21
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-09-09 19:50:34 +0100

    No need for styles in callback

--- pages/callback.js
+++ pages/callback.js
@@ -1,21 +1,11 @@
 import React, { useEffect } from 'react';
 import Auth from '../lib/Auth';
 import Router from 'next/router';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
 import SectionHeader from '../components/SectionHeader';
 
 const auth = new Auth();
 
-const styles = {
-  message: {
-    color: 'blue',
-    fontWeight: 'bold',
-    justifyContent: 'center',
-  },
-};
-
-const Callback = ({ classes }) => {
+const Callback = () => {
   useEffect(() => {
     const userDetails = auth.extractInfoFromHash();
     auth.handleAuthentication().then(res => {
@@ -27,17 +17,11 @@ const Callback = ({ classes }) => {
   }, []);
 
   return (
-    <div className={classes.message}>
-      <SectionHeader
-        title='Processing login information!!'
-        subtitle='Please wait!!'
-      />
-    </div>
+    <SectionHeader
+      title='Processing login information!!'
+      subtitle='Please wait!!'
+    />
   );
 };
 
-Callback.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Callback);
+export default Callback;
