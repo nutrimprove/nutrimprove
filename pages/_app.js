268c74cc3d1967b99fef4efe2c3d4dfafe4f747c
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-09-29 21:18:12 +0100

    Removed obsolete helpers file and moved withReduxStore

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from 'next/app';
 import React from 'react';
-import withReduxStore from '../lib/withReduxStore';
+import withReduxStore from '../store/withReduxStore';
 import { Provider } from 'react-redux';
 import Header from '../components/Header';
 
