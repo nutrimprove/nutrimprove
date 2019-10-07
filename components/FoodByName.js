6cfe765b3f0cff00106a746f1f5aaab7031f69c7
Joao Jesus <joao.jesus@blackcatsolutions.co.uk>
2019-10-07 23:14:13 +0100

    Implemented new nutritional results to Food by Name panel

--- components/FoodByName.js
+++ components/FoodByName.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import ResultsTable from './FoodFullResults';
-import TextField from '@material-ui/core/TextField';
-import { fetchFoods } from '../connect/api';
+import ResultsTable from './ResultsTable';
+import { getNutritionalData, getSearchedTerms } from '../connect/api';
 import SectionHeader from './SectionHeader';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import ButtonWithSpinner from './ButtonWithSpinner';
+import SearchFoodField from './SearchFoodField';
+import { trackPromise } from 'react-promise-tracker';
+import { INPUT_TRIGGER_TIME } from '../helpers/constants';
+
+let timeout = null;
 
 const sectionHeader = {
   title: 'Search food by name',
@@ -19,36 +23,76 @@ const styles = {
   },
 };
 
+const parseNutrients = nutrients => {
+  const nutrientsObj = [];
+  const keys = Object.keys(nutrients);
+  keys.map(key => {
+    const nutrient = nutrients[key];
+    nutrientsObj.push({
+      nutrient: nutrient.label,
+      quantity: `${Number.parseFloat(nutrient.quantity).toFixed(2)} (${
+        nutrient.unit
+      })`,
+    });
+  });
+  return nutrientsObj;
+};
+
 const FoodByName = ({ classes }) => {
-  const [foodName, setFoodName] = useState('');
-  const [foods, setFoods] = useState([]);
-  const [searched, setSearched] = useState(false);
+  const [food, setFood] = useState({
+    name: null,
+    id: null,
+    suggestions: [],
+  });
+  const [foodData, setFoodData] = useState();
+
+  const updateState = async (selectedFood, value) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(async () => {
+      const search = await trackPromise(
+        getSearchedTerms(value),
+        `getSearchTerms-${selectedFood.id}`
+      );
+
+      if (search && search.matches) {
+        const suggestions = search.matches.map(match => ({
+          food_name: match.food_name,
+          food_id: match.food_id,
+        }));
+        const selected = suggestions.find(
+          suggestion => suggestion.food_name === value
+        );
+        setFood({
+          name: selected.food_name,
+          id: selected.food_id,
+          suggestions,
+        });
+      }
+    }, INPUT_TRIGGER_TIME);
+  };
 
   const updateResults = async () => {
-    setFoods([]);
-    if (foodName !== '') {
-      const foods = await fetchFoods(foodName);
-      setFoods(foods);
-      setSearched(true);
-    }
+    const data = await getNutritionalData(food.id);
+    const nutrients = parseNutrients(data.totalNutrients);
+    setFoodData(nutrients);
   };
 
   return (
     <>
       <SectionHeader content={sectionHeader} />
-      <TextField
-        id='foodName'
-        label='Type food name'
-        type='search'
-        value={foodName}
-        className={classes.textField}
-        margin='normal'
-        onChange={e => setFoodName(e.target.value)}
+      <SearchFoodField
+        food={food}
+        loadingContext={`getSearchTerms-${food.id}`}
+        action={updateState}
       />
-      <ButtonWithSpinner action={updateResults} context='fetchFoods'>
+      <ButtonWithSpinner
+        action={updateResults}
+        context='getNutritionalData'
+        disabled={!food.id}
+      >
         Search
       </ButtonWithSpinner>
-      {searched && <ResultsTable values={foods} />}
+      {foodData && <ResultsTable values={foodData} />}
     </>
   );
 };
