37c026f6be77bff17d5e7311a45e3fb5ba66bd0d
Joao Jesus <johncjesus@gmail.com>
2020-03-18 21:24:46 +0000

    Updating user points in global state

--- store/global/actions.js
+++ store/global/actions.js
@@ -1,7 +1,12 @@
 export const ActionsTypes = {
   SET_USER_DETAILS: 'SET_USER_DETAILS',
+  ADD_USER_POINTS: 'ADD_USER_POINTS',
 };
 
 export const setUserDetails = userDetails => {
   return { type: ActionsTypes.SET_USER_DETAILS, userDetails };
+};
+
+export const addUserPointsAction = points => {
+  return { type: ActionsTypes.ADD_USER_POINTS, points };
 };
--- store/global/reducer.js
+++ store/global/reducer.js
@@ -1,10 +1,20 @@
 import { ActionsTypes } from './actions';
 
+const userWithAddedPoints = (userDetails, points) => {
+  if (points > 0 && userDetails && userDetails.points) {
+    userDetails.points = userDetails.points + points;
+    return userDetails;
+  }
+};
+
 export const reducer = (state = { userDetails: {} }, action) => {
   if (action.type === ActionsTypes.SET_USER_DETAILS) {
     return {
-      ...state,
       userDetails: action.userDetails,
+    };
+  } else if (action.type === ActionsTypes.ADD_USER_POINTS) {
+    return {
+      userDetails: userWithAddedPoints(state.userDetails, action.points),
     };
   } else {
     return state;
