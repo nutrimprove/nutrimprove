7553f7c05932084fac00e73d99608f3d104934c0
Joao Jesus <johncjesus@gmail.com>
2020-05-03 14:00:42 +0100

    Added TabbedPanel component

--- components/SearchFoodPage.js
+++ components/SearchFoodPage.js
@@ -1,92 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SectionHeader from './SectionHeader';
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types';
 import SearchFilters from './SearchFilters';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import TabContainer from './TabContainer';
 import SearchFoodByNutrient from './SearchFoodByNutrient';
 import SearchFoodByName from './SearchFoodByName';
 import { EDAMAM_DB } from '../helpers/constants';
 import SearchEdamamFoodByName from './SearchEdamamFoodByName';
+import TabbedPanel from './TabbedPanel';
 
 const sectionHeader = {
   title: 'Search food',
   subtitle: 'Search for a food to display its nutritional data (use the filters to refine your search)',
 };
 
-const SearchFoodPage = ({ classes }) => {
-  const [tab, setTab] = useState(0);
+const SearchFoodPage = () => {
+  const edamamTab = [{
+    label: 'Search by name',
+    content: <SearchEdamamFoodByName/>,
+  }];
 
-  const tabChange = (event, tab) => {
-    setTab(tab);
-  };
-
-  const tabbedPanelEdamam = () => (
-    <>
-      <Tabs indicatorColor='primary' value={tab} onChange={tabChange}>
-        <Tab className={classes.tab} label='Search by name'/>
-      </Tabs>
-      <TabContainer value={tab} index={0}>
-        <SearchEdamamFoodByName/>
-      </TabContainer>
-    </>
-  );
-
-  const tabbedPanel = () => (
-    <div className={classes.tabContent}>
-      <Tabs indicatorColor='secondary' value={tab} onChange={tabChange}>
-        <Tab className={classes.tab} label='Search by name'/>
-        <Tab className={classes.tab} label='Search by Nutrient'/>
-      </Tabs>
-      <TabContainer value={tab} index={0}>
-        <SearchFoodByName/>
-      </TabContainer>
-      <TabContainer value={tab} index={1}>
-        <SearchFoodByNutrient/>
-      </TabContainer>
-    </div>
-  );
+  const tabs = [
+    {
+      label: 'Search by name',
+      content: <SearchFoodByName/>,
+    },
+    {
+      label: 'Search by nutrient',
+      content: <SearchFoodByNutrient/>,
+    },
+  ];
 
   return (
     <>
       <SectionHeader content={sectionHeader}/>
       <SearchFilters/>
-      {EDAMAM_DB ? tabbedPanelEdamam() : tabbedPanel()}
+      {EDAMAM_DB
+        ? <TabbedPanel tabs={edamamTab}/>
+        : <TabbedPanel tabs={tabs}/>}
     </>
   );
 };
 
-SearchFoodPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-const styles = {
-  tab: {
-    '&:not(.Mui-selected)': {
-      backgroundColor: '#3F51B5',
-      color: 'lightgrey',
-    },
-    '&.Mui-selected': {
-      backgroundColor: '#3F51B5',
-      color: 'white',
-    },
-    borderColor: 'lightgrey',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRadius: '9px 9px 0 0',
-    fontSize: '0.7em',
-  },
-  tabContent: {
-    marginTop: 15,
-  },
-  container: {
-    borderColor: 'red',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRadius: '9px 9px 0 0',
-  },
-};
-
-export default withStyles(styles)(SearchFoodPage);
+export default SearchFoodPage;
---
+++ components/TabbedPanel.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles/index';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TabContainer from './TabContainer';
+
+const TabbedPanel = ({ tabs, classes }) => {
+  const [tabOption, setTabOption] = useState(0);
+
+  const tabChange = (event, tab) => {
+    setTabOption(tab);
+  };
+
+  return (
+    <div className={classes.tabContent}>
+      <Tabs indicatorColor='secondary' value={tabOption} onChange={tabChange}>
+        {tabs.map(tab => (
+          <Tab key={tab.label} className={classes.tab} label={tab.label}/>
+        ))}
+      </Tabs>
+      {tabs.map((tab, index) => (
+        <TabContainer key={`${tab.label}-content`} value={tabOption} index={index}>
+          {tab.content}
+        </TabContainer>
+      ))}
+    </div>
+  );
+};
+
+TabbedPanel.propTypes = {
+  tabs: PropTypes.array.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+const styles = {
+  tabContent: {
+    marginTop: 10,
+  },
+  tab: {
+    '&:not(.Mui-selected)': {
+      backgroundColor: '#3F51B5',
+      color: 'lightgrey',
+    },
+    '&.Mui-selected': {
+      backgroundColor: '#3F51B5',
+      color: 'white',
+    },
+    borderRadius: '9px 9px 0 0',
+    marginRight: 2,
+    fontSize: '0.7em',
+  },
+};
+
+export default withStyles(styles)(TabbedPanel);
