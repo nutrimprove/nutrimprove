040dee8e421e42be828ca54194383994af2945f9
Joao Jesus <johncjesus@gmail.com>
2020-05-03 20:32:56 +0100

    Moved food card into a component

--- components/AddRecommendations.js
+++ components/AddRecommendations.js
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, CardActions, CardContent, List, ListItem, Typography, withStyles } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, Typography, withStyles } from '@material-ui/core';
 import { getFoodById } from '../interfaces/api/foods';
 import ResultsModal from './ResultsModal';
 import { parseNutrients } from '../helpers/utils';
 import clsx from 'clsx';
-
-const essentialNutrients = [
-  { name: 'energy', label: 'Energy' },
-  { name: 'carbohydrate', label: 'Carbohydrate' },
-  { name: 'fat', label: 'Fat' },
-  { name: 'protein', label: 'Protein' },
-  { name: 'totalSugars', label: 'Total Sugar' },
-  { name: 'fibre', label: 'Fibre' },
-  { name: 'SFA', label: 'Saturated Fat' },
-  { name: 'omega3', label: 'Omega 3' },
-  { name: 'Cholesterol', label: 'Cholesterol' },
-];
+import FoodCard from './FoodCard';
 
 const AddRecommendations = ({ classes }) => {
   const [food, setFood] = useState();
@@ -35,35 +24,6 @@ const AddRecommendations = ({ classes }) => {
       setRecommendation(recommendationResult);
     })();
   }, []);
-
-  const essentialNutrientsInfo = (foodObj) => {
-    if (!foodObj) return;
-    const { proximates } = foodObj;
-
-    // Get only essential nutrients to display in the card
-    const nutrients = {};
-    Object.keys(proximates).map(key => {
-      const exists = essentialNutrients.some(nutrient => nutrient.name === key);
-      if (exists) {
-        nutrients[key] = proximates[key];
-      }
-    });
-
-    const parsedNutrients = parseNutrients({ nutrients, filterEmptyValues: false, addKey: true });
-    return (
-      <List className={classes.list}>
-        {essentialNutrients.map(nutrient => {
-          const parsedNutrient = parsedNutrients.find(parsedNutrient => parsedNutrient.key === nutrient.name);
-          return (
-            <ListItem key={name} className={classes.item}>
-              <span className={classes.nutrient}>{nutrient.label}</span>
-              <span className={classes.value}>{parsedNutrient.quantity}</span>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
-  };
 
   const loadFoodDetails = foodObj => {
     const { foodName, proximates, vitamins, inorganics } = foodObj;
@@ -93,42 +53,8 @@ const AddRecommendations = ({ classes }) => {
 
   return (
     <div className={classes.root}>
-      {food && <Card className={clsx(classes.card, classes.food)}>
-        <Typography className={classes.title} color="textSecondary" noWrap={true}>
-          {food.foodName}
-        </Typography>
-        <CardContent className={classes.content}>
-          {essentialNutrientsInfo(food)}
-        </CardContent>
-        <CardActions className={classes.actions}>
-          <Button
-            variant='outlined'
-            color='primary'
-            className={classes.button}
-            onClick={showFoodDetails}
-          >
-            Show more
-          </Button>
-        </CardActions>
-      </Card>}
-      {recommendation && <Card className={clsx(classes.card, classes.recommendation)}>
-        <Typography className={classes.title} color="textSecondary" title={recommendation.foodName} noWrap={true}>
-          {recommendation.foodName}
-        </Typography>
-        <CardContent className={classes.content}>
-          {essentialNutrientsInfo(recommendation)}
-        </CardContent>
-        <CardActions className={classes.actions}>
-          <Button
-            variant='outlined'
-            color='primary'
-            className={classes.button}
-            onClick={showRecommendationDetails}
-          >
-            Show more
-          </Button>
-        </CardActions>
-      </Card>}
+      <FoodCard food={food} onShowMoreClick={showFoodDetails}/>
+      <FoodCard food={recommendation} onShowMoreClick={showRecommendationDetails}/>
       {detailsOpen && <ResultsModal
         data={foodDetails.nutrients}
         open={detailsOpen}
@@ -147,50 +73,7 @@ AddRecommendations.propTypes = {
 const styles = {
   root: {
     display: 'inline-flex',
-  },
-  card: {
-    width: 400,
-    borderRadius: 7,
-  },
-  food: {
-    margin: 0,
-  },
-  recommendation: {
-    marginLeft: 50,
-  },
-  title: {
-    fontWeight: 600,
-    background: '#f5f5fa',
-    padding: 20,
-    borderBottom: '1px solid lightgrey',
-    textAlign: 'center',
-    margin: 0,
-  },
-  nutrient: {
-    display: 'inline',
-    fontWeight: 'bold',
-    zIndex: 1,
-  },
-  value: {
-    position: 'absolute',
-    right: 0,
-  },
-  list: {
-    fontFamily: 'sans-serif, arial',
-    fontSize: '0.8em',
-    padding: 0,
-  },
-  item: {
-    padding: '8px 0',
-    borderBottom: '1px dotted #ddd',
-  },
-  actions: {
-    padding: '0 14px',
-    marginBottom: 16,
-  },
-  button: {
-    fontSize: '0.7em',
-    height: 30,
+    minHeight: 430,
   },
 };
 
---
+++ components/FoodCard.js
@@ -0,0 +1,123 @@
+import { Button, Card, CardActions, CardContent, List, ListItem, Typography, withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { parseNutrients } from '../helpers/utils';
+
+const essentialNutrients = [
+  { name: 'energy', label: 'Energy' },
+  { name: 'carbohydrate', label: 'Carbohydrate' },
+  { name: 'fat', label: 'Fat' },
+  { name: 'protein', label: 'Protein' },
+  { name: 'totalSugars', label: 'Total Sugar' },
+  { name: 'fibre', label: 'Fibre' },
+  { name: 'SFA', label: 'Saturated Fat' },
+  { name: 'omega3', label: 'Omega 3' },
+  { name: 'Cholesterol', label: 'Cholesterol' },
+];
+
+const FoodCard = ({ food, onShowMoreClick, classes }) => {
+  const title = food ? food.foodName : '';
+  const essentialNutrientsInfo = (foodObj) => {
+    if (!foodObj) return;
+    const { proximates } = foodObj;
+
+    // Get only essential nutrients to display in the card
+    const nutrients = {};
+    Object.keys(proximates).map(key => {
+      const exists = essentialNutrients.some(nutrient => nutrient.name === key);
+      if (exists) {
+        nutrients[key] = proximates[key];
+      }
+    });
+
+    const parsedNutrients = parseNutrients({ nutrients, filterEmptyValues: false, addKey: true });
+    return (
+      <List className={classes.list}>
+        {essentialNutrients.map(nutrient => {
+          const parsedNutrient = parsedNutrients.find(parsedNutrient => parsedNutrient.key === nutrient.name);
+          return (
+            <ListItem key={name} className={classes.item}>
+              <span className={classes.nutrient}>{nutrient.label}</span>
+              <span className={classes.value}>{parsedNutrient.quantity}</span>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+
+  return (
+    <Card className={classes.card}>
+      <Typography className={classes.title} color="textSecondary" title={title} noWrap={true}>
+        {title}
+      </Typography>
+      <CardContent className={classes.content}>
+        {essentialNutrientsInfo(food)}
+      </CardContent>
+      <CardActions className={classes.actions}>
+        <Button
+          variant='outlined'
+          color='primary'
+          className={classes.button}
+          onClick={onShowMoreClick}
+        >
+          Show more
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+FoodCard.propTypes = {
+  food: PropTypes.object.isRequired,
+  onShowMoreClick: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+const styles = {
+  card: {
+    width: 400,
+    borderRadius: 7,
+    marginRight: 50,
+  },
+  content: {
+    minHeight: 288,
+  },
+  title: {
+    fontWeight: 600,
+    minHeight: 24,
+    background: '#f5f5fa',
+    padding: 20,
+    borderBottom: '1px solid lightgrey',
+    textAlign: 'center',
+    margin: 0,
+  },
+  nutrient: {
+    display: 'inline',
+    fontWeight: 'bold',
+    zIndex: 1,
+  },
+  value: {
+    position: 'absolute',
+    right: 0,
+  },
+  list: {
+    fontFamily: 'sans-serif, arial',
+    fontSize: '0.8em',
+    padding: 0,
+  },
+  item: {
+    padding: '8px 0',
+    borderBottom: '1px dotted #ddd',
+  },
+  actions: {
+    padding: '0 14px',
+    marginBottom: 16,
+  },
+  button: {
+    fontSize: '0.7em',
+    height: 30,
+  },
+};
+
+export default withStyles(styles)(FoodCard);
