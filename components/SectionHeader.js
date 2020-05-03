4505ccb483b874908a317b069e129c6f41150bb2
Joao Jesus <johncjesus@gmail.com>
2020-05-03 14:02:50 +0100

    Deleted obsolete styles class

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -3,31 +3,6 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { uniqueId } from 'lodash';
 
-const styles = {
-  header: {
-    marginBottom: '30px',
-    fontFamily: 'sans-serif, arial',
-    backgroundColor: '#fafafd',
-    padding: '10px',
-    border: 'solid 1px lightgrey',
-    borderRadius: 9,
-    fontSize: '0.9em',
-    fontWeight: 200,
-    lineHeight: '1.5em',
-    textAlign: 'center',
-  },
-  title: {
-    fontSize: '1.3em',
-    marginBottom: 10,
-    fontFamily: 'tahome, sans-serif, arial',
-    fontWeight: 400,
-  },
-  fetchButton: {
-    verticalAlign: 'bottom',
-    marginLeft: 10,
-  },
-};
-
 const SectionHeader = ({ content, classes }) => {
   return (
     <div className={classes.header}>
@@ -47,6 +22,27 @@ const SectionHeader = ({ content, classes }) => {
 SectionHeader.propTypes = {
   content: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
+};
+
+const styles = {
+  header: {
+    marginBottom: '30px',
+    fontFamily: 'sans-serif, arial',
+    backgroundColor: '#fafafd',
+    padding: '10px',
+    border: 'solid 1px lightgrey',
+    borderRadius: 9,
+    fontSize: '0.9em',
+    fontWeight: 200,
+    lineHeight: '1.5em',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: '1.3em',
+    marginBottom: 10,
+    fontFamily: 'tahome, sans-serif, arial',
+    fontWeight: 400,
+  },
 };
 
 export default withStyles(styles)(SectionHeader);
