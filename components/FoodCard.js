b1cd64ff6be16b0993ef6f21ef6a0d146692d3e7
Joao Jesus <johncjesus@gmail.com>
2020-05-04 19:25:39 +0100

    Scroll card into view when loaded

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -1,11 +1,19 @@
 import { Button, Card, CardActions, CardContent, List, ListItem, Typography, withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import { uniqueId } from 'lodash/util';
 
 const FoodCard = ({ food, onShowMoreClick, onMouseOver, highlightItem, classes }) => {
+  const ref = useRef();
   const title = food ? food.foodName : '';
+
+  useEffect(() => {
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }, []);
 
   return (
     <Card className={classes.card}>
@@ -33,6 +41,7 @@ const FoodCard = ({ food, onShowMoreClick, onMouseOver, highlightItem, classes }
           color='primary'
           className={classes.button}
           onClick={onShowMoreClick}
+          ref={ref}
         >
           Show more
         </Button>
