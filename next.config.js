c13499f303925aab95dd782ad8a29d8875ffe4cd
Joao Jesus <johncjesus@gmail.com>
2020-05-25 21:28:22 +0100

    Removed not needed line in next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const Dotenv = require('dotenv-webpack');
-const path = require('path');
 
 module.exports = {
   target: 'serverless',
   webpack(config) {
     config.node = { fs: 'empty', tls: 'empty', net: 'empty' };
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
     config.plugins.push(new Dotenv());
     return config;
   },
