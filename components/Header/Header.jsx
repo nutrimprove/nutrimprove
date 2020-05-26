3529eaf65b9f340cfaed7dce3685ba2cb65be787
Joao Jesus <johncjesus@gmail.com>
2020-05-26 16:58:05 +0100

    Refactored authentication to be done exclusively in a single location (Header)

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,30 +1,59 @@
 import { AppBar, Link, Toolbar, Typography } from '@material-ui/core';
 import { EDAMAM_DB } from 'helpers/constants';
-import { setUserState } from 'helpers/userUtils';
 import { getAllFoodNames } from 'interfaces/api/foods';
+import { getUser } from 'interfaces/api/users';
 import Auth from 'interfaces/auth/Auth';
 import PropTypes from 'prop-types';
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { getStore } from 'store';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFoodNamesAction, setUserDetailsAction, setUserPreferencesAction } from 'store/global/actions';
 import HeaderLink from './HeaderLink';
 
 const auth = new Auth();
+const dispatch = (action) => getStore().dispatch(action);
+
+const processAuth = () => {
+  try {
+    const userDetails = auth.extractInfoFromHash();
+    const { user_details: userInfo } = userDetails;
+    auth.handleAuthentication().then(async res => {
+      if (res) {
+        if (userInfo && userInfo.email) {
+          const user = await getUser(userInfo.email);
+          if (user) {
+            const { role, approved, points, preferences } = user;
+            dispatch(setUserDetailsAction({ ...userInfo, role, approved, points }));
+            dispatch(setUserPreferencesAction(preferences));
+          } else {
+            console.error('User not found!', userInfo.email);
+          }
+        } else {
+          console.error('UserInfo object not valid!', userInfo);
+        }
+        if (typeof window !== 'undefined') {
+          localStorage.setItem(
+            'token',
+            JSON.stringify(userDetails.token),
+          );
+        }
+      }
+    });
+  } catch (e) {
+    console.warn(`No authentication found!`);
+  }
+};
 
 const Header = ({ classes }) => {
   const { userDetails } = useSelector(state => state.globalState);
   const dispatch = useDispatch();
-  const setUserPreferences = useCallback(preferences => dispatch(setUserPreferencesAction(preferences)), []);
-  const setUserDetails = useCallback(details => dispatch(setUserDetailsAction(details)), []);
-  const setFoodNames = useCallback(foodNames => dispatch(setFoodNamesAction(foodNames)), []);
 
   useEffect(() => {
     (async () => {
-      const userInfo = auth.extractUserFromToken();
-      setUserState({ setUserDetails, setUserPreferences, userInfo });
+      await processAuth();
       if (!EDAMAM_DB) {
         const foodNames = await getAllFoodNames();
-        setFoodNames(foodNames);
+        dispatch(setFoodNamesAction(foodNames));
       }
     })();
   }, []);
