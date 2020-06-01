906611fdb10e1554f2df51397ec7a1b90f5da324
Joao Jesus <johncjesus@gmail.com>
2020-06-01 17:56:17 +0100

    Added Google Analytics

---
+++ helpers/analytics.js
@@ -0,0 +1,12 @@
+const trackPageView = url => {
+  try {
+    window.gtag('config', 'UA-XXXXXXXX-X', {
+      page_location: url,
+    });
+  } catch (error) {
+    // silences the error in dev mode
+    // and/or if gtag fails to load
+  }
+};
+
+export { trackPageView };
--- pages/_document.js
+++ pages/_document.js
@@ -30,28 +30,52 @@ export default class MyDocument extends Document {
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
-
     ctx.renderPage = () =>
       originalRenderPage({
         enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
       });
 
+    const isProduction = process.env.NODE_ENV === 'production';
     const initialProps = await Document.getInitialProps(ctx);
 
     return {
       ...initialProps,
+      isProduction,
       // Styles fragment is rendered after the app and page rendering finish.
       styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
     };
   }
 
+  setGoogleTags() {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-168058659-2');
+      `,
+    };
+  }
+
   render() {
+    const { isProduction } = this.props;
+
     return (
       <Html lang="en">
-        <Head />
+        <Head/>
         <body>
         <Main/>
         <NextScript/>
+        {isProduction && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=UA-168058659-2"
+            />
+            {/* We call the function above to inject the contents of the script tag */}
+            <script dangerouslySetInnerHTML={this.setGoogleTags()}/>
+          </>
+        )}
         </body>
       </Html>
     );
