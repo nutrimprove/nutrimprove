82e99f4330463de22c2f50220b2a7e51cabe4571
Joao Jesus <johncjesus@gmail.com>
2020-06-03 00:34:23 +0100

    Removed non currently needed LoadingOverlay component

--- components/LoadingOverlay/index.js
+++
@@ -1,5 +0,0 @@
-import { withStyles } from '@material-ui/core';
-import LoadingOverlay from './LoadingOverlay';
-import styles from './LoadingOverlay.styles';
-
-export default withStyles(styles)(LoadingOverlay);
