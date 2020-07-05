f775b3acabe548bf9e4852c959ca59c64aa86123
Joao Jesus <johncjesus@gmail.com>
2020-07-05 15:31:36 +0100

    Added RightNavText component

---
+++ components/Header/MainNav/RightNavText/RightNavText.styles.js
@@ -0,0 +1,13 @@
+export default {
+  rightNavContent: {
+    fontSize: 14,
+    marginLeft: 'auto',
+  },
+  notice: {
+    color: 'yellow',
+  },
+  rightNavText: {
+    marginLeft: 12,
+    verticalAlign: 'text-bottom',
+  },
+};
