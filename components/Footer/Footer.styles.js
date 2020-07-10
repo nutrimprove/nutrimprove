ea5055f5907064d18219cbf93f6f85c114cde2b3
Joao Jesus <johncjesus@gmail.com>
2020-07-10 15:18:20 +0100

    Added Footer

---
+++ components/Footer/Footer.styles.js
@@ -0,0 +1,19 @@
+import { MIN_WIDTH } from 'helpers/constants';
+
+export default {
+  footer: {
+    minWidth: MIN_WIDTH,
+    minHeight: 20,
+    margin: '20px -20px',
+    backgroundColor: '#3f51b5',
+    padding: 10,
+    borderRadius: '0 0 7px 7px',
+  },
+  container: {
+    color: 'white',
+  },
+  copyright: {
+    float: 'right',
+    marginRight: 10,
+  }
+};
