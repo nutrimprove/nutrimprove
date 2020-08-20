ec902843b2c0151d5e35ab70b12b52e836576e9a
Joao Jesus <johncjesus@gmail.com>
2020-08-20 19:05:38 +0100

    Removed obsolete SearchInput component (WOOHOO)

--- components/AddBulkRecommendations/SearchInputField/index.js
+++
@@ -1,5 +0,0 @@
-import { withStyles } from '@material-ui/core';
-import SearchInputField from 'components/AddBulkRecommendations/SearchInputField/SearchInputField';
-import styles from 'components/AddBulkRecommendations/SearchInputField/SearchInputField.styles';
-
-export default withStyles(styles)(SearchInputField);
