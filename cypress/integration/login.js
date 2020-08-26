0814aab48269395c3c31a469a47795b853a7bef2
Joao Jesus <johncjesus@gmail.com>
2020-08-26 18:37:52 +0100

    Added cypress and updated vercel/now

---
+++ cypress/integration/login.js
@@ -0,0 +1,5 @@
+describe('login', () => {
+  it('should successfully login', () => {
+    cy.login();
+  });
+});
---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,10 @@
+
+describe('Search food', () => {
+  before(() => {
+    cy.login();
+  });
+
+  it('Loads', () => {
+    expect(true).to.equal(true);
+  });
+});
