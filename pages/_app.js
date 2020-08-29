1dad7492a171bffbc1bb8400c2230600f9fdda0c
Joao Jesus <johncjesus@gmail.com>
2020-08-29 20:12:11 +0100

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,8 @@ import { AuthProvider } from 'react-use-auth';
 import withReduxStore from 'store/withReduxStore';
 
 const noHeaderPages = ['/info'];
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const AUTH0_CLIENT = process.env.AUTH0_CLIENT_ID;
 
 const MyApp = ({ Component, pageProps, store }) => {
   const router = useRouter();
@@ -29,9 +31,14 @@ const MyApp = ({ Component, pageProps, store }) => {
   return (
     <AuthProvider
       navigate={router.push}
-      auth0_domain={process.env.AUTH0_DOMAIN}
-      auth0_client_id={process.env.AUTH0_CLIENT_ID}
+      auth0_domain={AUTH0_DOMAIN}
+      auth0_client_id={AUTH0_CLIENT}
     >
+      {console.log(`=== _app.js #37 === ( AUTH0_DOMAIN ) =======>`, AUTH0_DOMAIN)}
+      {console.log(`=== _app.js #38 === ( AUTH0_CLIENT ) =======>`, AUTH0_CLIENT)}
+      {console.log(`=== _app.js #36 === ( process.env.MONGODB_URI ) =======>`, process.env.MONGODB_URI)}
+      {console.log(`=== _app.js #36 === ( process.env.AUTH0_CLIENT_ID ) =======>`, process.env.AUTH0_CLIENT_ID)}
+      {console.log(`=== _app.js #36 === ( process.env.AUTH0_DOMAIN ) =======>`, process.env.AUTH0_DOMAIN)}
       <Provider store={store}>
         <LoaderContainer>
           {hasHeader ? <Header/> : null}
