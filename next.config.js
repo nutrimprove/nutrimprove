347fce89518e3d7cb1f27cbe3bb99dc8a96bce92
Joao Jesus <johncjesus@gmail.com>
2020-08-29 21:35:49 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const Dotenv = require('dotenv-webpack');
-
-module.exports = {
-  target: 'serverless',
-  webpack(config) {
-    config.node = { fs: 'empty', tls: 'empty', net: 'empty' };
-    config.plugins.push(new Dotenv());
-    return config;
-  },
-};
+// const Dotenv = require('dotenv-webpack');
+//
+// module.exports = {
+//   target: 'serverless',
+//   webpack(config) {
+//     config.node = { fs: 'empty', tls: 'empty', net: 'empty' };
+//     config.plugins.push(new Dotenv());
+//     return config;
+//   },
+// };
