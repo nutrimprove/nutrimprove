11381b1c601b5b9a889343696a86aacf735fe35f
Joao Jesus <johncjesus@gmail.com>
2020-08-30 10:38:13 +0100

    Add env to next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-// const Dotenv = require('dotenv-webpack');
-//
-// module.exports = {
-//   target: 'serverless',
-//   webpack(config) {
-//     config.node = { fs: 'empty', tls: 'empty', net: 'empty' };
-//     config.plugins.push(new Dotenv());
-//     return config;
-//   },
-// };
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  target: 'serverless',
+  webpack(config) {
+    config.node = { fs: 'empty', tls: 'empty', net: 'empty' };
+    config.plugins.push(new Dotenv({ silent: true }));
+    return config;
+  },
+  env: {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  },
+};
