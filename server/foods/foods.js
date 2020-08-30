549af96072a4c06738069acb550819897be4179e
Joao Jesus <johncjesus@gmail.com>
2020-08-30 12:50:47 +0100

    test

--- server/foods/foods.js
+++ server/foods/foods.js
@@ -47,6 +47,7 @@ const getNutrients = async (nutrientGroups) => {
   });
 
   const result = await FoodsConnection.findOne({}, projection);
+  console.log(`=== foods.js #50 === ( result ) =======>`, result);
   const document = result._doc;
   const nutrientsList = [];
 
