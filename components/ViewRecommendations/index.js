bca5ad0253e865ceb2dc5d44d4861b2aa1d255e8
Joao Jesus <johncjesus@gmail.com>
2020-10-08 19:25:29 +0100

    Added styles to ViewRecommendations

--- components/ViewRecommendations/index.js
+++ components/ViewRecommendations/index.js
@@ -1,1 +1,5 @@
-export { default } from './ViewRecommendations';
+import { withStyles } from '@material-ui/core';
+import styles from './ViewRecommendations.styles';
+import ViewRecommendations from './ViewRecommendations';
+
+export default withStyles(styles)(ViewRecommendations);
