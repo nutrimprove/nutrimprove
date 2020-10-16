aa9db99abd22e4487beee3e0bcc72af980b32ad3
Joao Jesus <johncjesus@gmail.com>
2020-10-16 18:48:37 +0100

    Implemented actions for Healthy Selection screen

--- components/HealthySelection/HealthySelection.jsx
+++ components/HealthySelection/HealthySelection.jsx
@@ -1,40 +1,95 @@
+import ButtonWithSpinner from 'components/ButtonWithSpinner';
 import CardTitle from 'components/CardTitle';
 import FoodCard from 'components/FoodCard';
 import LoadingPanel from 'components/LoadingPanel';
-import { getFoodById, getHealthyUnflaggedFoods } from 'interfaces/api/foods';
+import LoadingSpinner from 'components/LoadingSpinner';
+import { getFoodById, getHealthyUnflaggedFoods, setHealthyFlag } from 'interfaces/api/foods';
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { usePromiseTracker } from 'react-promise-tracker';
 
 const HealthySelection = ({ classes }) => {
   const [unflaggedFoods, setUnflaggedFoods] = useState();
-  const [index, setIndex] = useState();
   const [food, setFood] = useState();
+  const [noMoreFoods, setNoMoreFoods] = useState(false);
   const { promiseInProgress: loadingUnflaggedFoods } = usePromiseTracker({ area: 'getFlaggedFoods' });
   const { promiseInProgress: loadingFoodData } = usePromiseTracker({ area: 'getFoodData' });
-
-  const loading = loadingFoodData || loadingUnflaggedFoods || !food;
+  const foodIndex = useRef(0);
+  const loading = loadingUnflaggedFoods || !food;
 
   useEffect(() => {
     (async () => {
       const results = await getHealthyUnflaggedFoods();
-      if (results) {
-        setUnflaggedFoods(results);
-        setIndex(0);
-        // Fetch first unflagged food
-        const currentFood = await getFoodById(results[0].foodCode);
-        setFood(currentFood);
-      }
+      setUnflaggedFoods(results);
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      if (unflaggedFoods) {
+        await loadFood();
+      }
+    })();
+  }, [unflaggedFoods]);
+
+  const loadFood = async () => {
+    if (foodIndex.current < unflaggedFoods.length) {
+      const currentFood = await getFoodById(unflaggedFoods[foodIndex.current].foodCode);
+      setFood(currentFood);
+      foodIndex.current = foodIndex.current + 1;
+    } else {
+      setNoMoreFoods(true);
+    }
+  };
+
+  const setHealthy = async () => {
+    setHealthyFlag(food.foodCode, true);
+    loadFood();
+  };
+
+  const setNonHealthy = async () => {
+    setHealthyFlag(food.foodCode, false);
+    loadFood();
+  };
+
+  const unflag = () => {
+    setHealthyFlag(food.foodCode, null);
+    loadFood();
+  };
+
   return (
     <>
-      {loading && <LoadingPanel/>}
-      {food && <div className={classes.card}>
-        <CardTitle title='Food'/>
-        <FoodCard food={food}/>
+      {loading && !noMoreFoods && <LoadingPanel/>}
+      {food && unflaggedFoods && !noMoreFoods && <div className={classes.container}>
+        <div className={classes.card}>
+          <CardTitle title='Food'>
+            <span className={classes.foodLoading}>
+              <LoadingSpinner context='getFoodData' size={18}/>
+            </span>
+          </CardTitle>
+          <FoodCard food={food}/>
+        </div>
+        <div className={classes.options}>
+          <ButtonWithSpinner className={classes.button} context='' action={setHealthy} colour='green' disabled={loadingFoodData}>
+            Healthy
+          </ButtonWithSpinner>
+          <ButtonWithSpinner className={classes.button} context='' action={setNonHealthy} disabled={loadingFoodData} colour='secondary'>
+            Not healthy
+          </ButtonWithSpinner>
+          <ButtonWithSpinner className={classes.button} context='' action={unflag} disabled={loadingFoodData}>
+            Clear healthy flag
+          </ButtonWithSpinner>
+          <ButtonWithSpinner
+            className={classes.button}
+            action={loadFood}
+            disabled={foodIndex.current >= unflaggedFoods.length - 1 || loadingFoodData}
+            disabledText={!loadingFoodData && 'No more foods'}
+          >
+            Skip
+          </ButtonWithSpinner>
+        </div>
       </div>}
+      {noMoreFoods && <div>No more foods!!</div>}
     </>
   );
 };
--- components/HealthySelection/HealthySelection.styles.js
+++ components/HealthySelection/HealthySelection.styles.js
@@ -1,4 +1,7 @@
 export default {
+  container: {
+    display: 'inline-flex',
+  },
   card: {
     width: 430,
     margin: 'auto',
@@ -8,6 +11,20 @@ export default {
     },
     '&:last-child': {
       margin: '0 auto 0 30px',
-    }
+    },
+  },
+  options: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'center',
+    flexWrap: 'wrap',
+  },
+  button: {
+    margin: 5,
+    width: 220,
+  },
+  foodLoading: {
+    position: 'absolute',
   },
 };
