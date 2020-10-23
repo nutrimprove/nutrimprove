1acee9d843d3410eaa41094dd59411f96330001c
Joao Jesus <johncjesus@gmail.com>
2020-10-23 21:50:04 +0100

    Added contributors dropdown to ViewRecommendations

--- components/ViewRecommendations/ViewRecommendations.jsx
+++ components/ViewRecommendations/ViewRecommendations.jsx
@@ -1,12 +1,15 @@
+import Link from '@material-ui/core/Link';
 import CompareModal from 'components/CompareModal';
 import LoadingPanel from 'components/LoadingPanel';
 import RadioOptions from 'components/RadioOptions';
 import ResultsTable from 'components/ResultsTable';
 import SearchField from 'components/SearchField';
+import { isAdmin } from 'helpers/userUtils';
 import { getFoodsFromRecommendation, parseFoodDetails } from 'helpers/utils';
 import { getRecommendation } from 'interfaces/api/recommendations';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const formatRecommendations = recommendations => {
   // Sort by date of recommendation (more recent first)
@@ -19,9 +22,22 @@ const formatRecommendations = recommendations => {
   }));
 };
 
+const getContributors = recommendations => {
+  const idList = [];
+  recommendations.forEach(rec => {
+    rec.contributors.forEach(({ id }) => {
+      if (!idList.includes(id)) {
+        idList.push(id);
+      }
+    });
+  });
+  return idList.map(id => ({ id }));
+};
+
 const hasFood = (foods, food) => !!foods.some(({ foodName }) => foodName === food.foodName);
 
 const ViewRecommendations = ({ recommendations, title, classes }) => {
+  const userDetails = useSelector(({ globalState }) => globalState.userDetails);
   const [list, setList] = useState();
   const [filteredList, setFilteredList] = useState();
   const [comparisonData, setComparisonData] = useState();
@@ -31,14 +47,19 @@ const ViewRecommendations = ({ recommendations, title, classes }) => {
   const [foodNames, setFoodNames] = useState();
   const [columnFilter, setColumnFilter] = useState('All');
   const [filterTriggered, setFilterTriggered] = useState(false);
+  const [contributors, setContributors] = useState();
+  const [contributor, setContributor] = useState();
+  const [showContributors, setShowContributors] = useState();
 
   const columnOptions = ['All', 'Foods', 'Recommendations'];
 
   const includeFoods = () => ['All', 'Foods'].includes(columnFilter);
   const includeRecommendations = () => ['All', 'Recommendations'].includes(columnFilter);
+  const getTitle = filtered => `${filtered.length} recommendations (out of ${list.length})`;
 
   useEffect(() => {
     if (!recommendations) return;
+    setContributors(getContributors(recommendations));
     const formattedRecommendations = formatRecommendations(recommendations);
     const foods = [];
     formattedRecommendations.forEach(rec => {
@@ -96,9 +117,22 @@ const ViewRecommendations = ({ recommendations, title, classes }) => {
   const filterByFood = () => {
     const filteredRecommendations = list.filter(({ food, recommendation }) =>
       (includeFoods() && food === filter) || (includeRecommendations() && recommendation === filter));
-    const title = `${filteredRecommendations.length} recommendations (out of ${list.length})`;
-    setFilteredList({ title, list: filteredRecommendations });
+    setFilteredList({ title: getTitle(filteredRecommendations), list: filteredRecommendations });
     setFilterTriggered(true);
+  };
+
+  const handleContributorSelection = (event, user) => {
+    setContributor(user.id);
+  };
+
+  const filterRecsByContributor = () => {
+    if (!recommendations) return;
+    // Get recommendations from selected contributor
+    const filtered = contributor
+      ? recommendations.filter(({ contributors }) => contributors.some(({ id }) => id === contributor))
+      : recommendations;
+    const formattedRecommendations = formatRecommendations(filtered);
+    setFilteredList({ title: getTitle(formattedRecommendations), list: formattedRecommendations });
   };
 
   return (
@@ -115,6 +149,25 @@ const ViewRecommendations = ({ recommendations, title, classes }) => {
         <RadioOptions options={columnOptions} onChange={handleColumnFilterChange} initialValue={columnFilter}
                       inline={false} label='Show from:' className={classes.filters}/>
       </div>
+      {isAdmin(userDetails) && <div>
+        <Link component='button' onClick={() => setShowContributors(!showContributors)}
+              className={classes.contributorsLabel}>
+          {showContributors ? 'Hide contributors' : 'Show contributors'}
+        </Link>
+        {showContributors && <SearchField onSelection={handleContributorSelection}
+                                          onButtonClick={filterRecsByContributor}
+                                          values={contributors}
+                                          buttonDisabled={!contributor}
+                                          label='Choose contributor'
+                                          labelProp='id'
+                                          noMatchText='No contributor matched!!'
+                                          strict={false}
+                                          hideDropdownIcon={false}
+                                          className={classes.search}
+        />}
+      </div>
+      }
+
       {!recommendations && <LoadingPanel/>}
       {filteredList && filteredList.list && <ResultsTable data={filteredList.list}
                                                           title={filteredList.title || title}
